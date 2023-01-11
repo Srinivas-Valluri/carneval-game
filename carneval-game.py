d05fd31cba8b9daaf04ff49b14db2cec0075ee50
Temp Login <vallurisrinivas09@gmail.com>
2023-01-11 23:35:03 +0530

    DEBUG
    Going into infinite loop when we guess correct

---
+++ carneval-game.py
@@ -0,0 +1,28 @@
+from random import shuffle
+
+l=['','O','']
+
+def shuffler(l):
+    shuffle(l)
+    return l
+
+def enter():
+    num=int(input("Pick '0', '1', '2': "))
+    while num in [0,1,2]:
+        checker(num,l)
+    print('Choose a valid number:')
+    enter()
+
+def checker(num,l):
+    if l[num]=='O':
+        print('You guessed it right!')
+        print(f'the list is {l} and ur guess is {num}')
+        return
+    else:
+        print('Wrong Guess')
+        print(f'the list is {l} and ur guess is {num}')
+        shuffler(l)
+        enter()
+    
+
+enter()
